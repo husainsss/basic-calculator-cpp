14faa25a6ab55f5509cba4075d12cbeac538bfca
husainsss <mhsanginov@gmail.com>
2024-07-28 17:52:01 +0500

    update v0.1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
+float global_value = 0;
+
+void hello() {
+    cout << "Hello World";
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
